814145337c18bf4d94a2e74d75d2992fe2bb8ec0
Alexis <alexis.vallet@gmail.com>
2014-10-15 01:03:43 +0900

    Properly implemented and tested a basic preprocessing pipeline on MNIST.

---
+++ preprocessing.py
@@ -0,0 +1,105 @@
+""" Dataset preprocessing and extension.
+"""
+from dataset import Dataset, DatasetMixin
+import cv2
+import numpy as np
+import theano
+
+class DatasetTransformer:
+    """ A dataset transformer transforms datasets (duh) for both training and
+        testing, under the condition that the test transform is a functor in
+        the category of pure functions (i.e. it is a sample-wise transform)
+        taking as additional input info from training data.
+    """
+    def train_data_transform(self, dataset):
+        """ Transforms a dataset for training purposes.
+        """
+        raise NotImplementedError()
+
+    def test_sample_transform(self, sample):
+        """ Transform a single sample for test purposes.
+        """
+        raise NotImplementedError()
+
+class DatasetTransformerMixin:
+    class BaseTestDataset(Dataset):
+        def __init__(self, parent, dataset):
+            self.dataset = dataset
+            self.sample_shape = dataset.sample_shape
+            self.parent = parent
+
+        def __iter__(self):
+            for sample_data in self.dataset:
+                sample, data = sample_data
+                yield (self.parent.test_sample_transform(sample), data)
+
+        def __len__(self):
+            return len(self.dataset)
+
+        def shuffle(self, permutation):
+            self.dataset.shuffle(permutation)
+
+    class TestDataset(BaseTestDataset, DatasetMixin):
+        pass
+    
+    def test_data_transform(self, dataset):
+        return self.TestDataset(self, dataset)
+
+class BaseMeanSubtraction(DatasetTransformer):
+    """ Dataset transformer for mean pixel value subtraction.
+    """
+    def __init__(self):
+        pass
+    
+    class BaseTrainMeanSubtraction(Dataset):
+        def __init__(self, dataset):
+            assert len(dataset.sample_shape) == 3
+            self.dataset = dataset
+            self.sample_shape = dataset.sample_shape
+    
+        def __iter__(self):
+            # First accumulate the mean pixel value.
+            self.mean_pixel = np.zeros(
+                [self.dataset.sample_shape[0], 1, 1],
+                np.float64
+            )
+            nb_samples = len(self.dataset)
+            new_shape = ([self.dataset.sample_shape[0], 
+                        np.prod(self.dataset.sample_shape[1:])])
+            
+            for image_data in self.dataset:
+                image, data = image_data
+                self.mean_pixel += np.mean(
+                    image.reshape(new_shape),
+                    axis=1,
+                    keepdims=True
+                ).astype(np.float64) / nb_samples
+            
+            self.mean_pixel = self.mean_pixel.astype(theano.config.floatX)
+            # Then subtract it from each image.
+            for image_data in self.dataset:
+                image, data = image_data
+                yield (image - self.mean_pixel, data)
+
+        def __len__(self):
+            return len(self.dataset)
+    
+        def shuffle(self, permutation):
+            self.dataset.shuffle(permutation)
+
+    class TrainMeanSubtraction(BaseTrainMeanSubtraction, DatasetMixin):
+        pass
+
+    def train_data_transform(self, dataset):
+        self.trainset = self.TrainMeanSubtraction(dataset)
+
+        return self.trainset
+
+    def test_sample_transform(self, sample):
+        """ The test transform simply subtracts the mean of the training data to the test
+            sample.
+        """
+        return sample - self.trainset.mean_pixel
+    
+class MeanSubtraction(BaseMeanSubtraction, DatasetTransformerMixin):
+    pass
