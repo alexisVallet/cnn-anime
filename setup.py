52f7e5a137e440ba1f18f079d56a55cebe45ede9
Alexis <alexis.vallet@gmail.com>
2015-02-10 14:11:32 +0900

    Working setuptools package.

--- setup.py
+++ setup.py
@@ -4,5 +4,18 @@ setup(
     name = "cnn_anime",
     version = "1.0",
     packages = find_packages(),
-    dependency_links=['git+git://github.com/Theano/Theano.git']
+    setup_requires=[
+        'numpy',
+        'six'
+    ],
+    install_requires=[
+        'numpy',
+        'six',
+        'Pillow',
+        'scikit-image',
+        'Theano'
+    ],
+    dependency_links=[
+        'git+git://github.com/Theano/Theano.git#egg=Theano'
+    ]
 )
